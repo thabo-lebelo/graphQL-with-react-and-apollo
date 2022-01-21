fa65544e4ddbfef9be352560df273b6d181f477f
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-21 19:36:14 +0200

    Add rocket type query

--- schema.js
+++ schema.js
@@ -54,6 +54,25 @@ const RootQuery = new GraphQLObjectType({
                 return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
                     .then((res) => res.data)
             }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args) {
+                console.log(parent);
+                console.log(args);
+                return axios.get(`https://api.spacexdata.com/v3/rockets`)
+                    .then((res) => res.data)
+            }
+        },
+        rocket: { 
+            type: RocketType,
+            args: {
+                 rocket_id : { type: GraphQLString}
+            },
+            resolve(parent, args) {
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+                    .then((res) => res.data)
+            }
         }
     }
 });
