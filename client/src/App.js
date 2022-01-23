e539b1d2d4b931a48fcdd4ff7af1d8b2dbafd4ae
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-23 18:32:04 +0200

    Fix styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Launches from './components/Launches'
 import './App.css';
+import logo from './logo.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto', marginTop: 20 }} />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
