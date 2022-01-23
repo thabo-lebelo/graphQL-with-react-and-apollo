fdc74d919f055b03555c7ef9aec4048755061995
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-23 20:21:25 +0200

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Launches from './components/Launches'
 import './App.css';
 import logo from './logo.png';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache()
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className="container">
-        <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto', marginTop: 20 }} />
-        <Launches />
-      </div>
-    </ApolloProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div className="container">
+                    <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto', marginTop: 20 }} />
+                    <Switch>
+                        <Route exact path="/"><Launches /></Route>
+                    </Switch>
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
